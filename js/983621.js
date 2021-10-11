e5b236dc899de87a0c2cbc071c67368d7df3725b
tjhcomputing <86693306+tjhcomputing@users.noreply.github.com>
2021-10-11 14:03:28 +0100

    Update 983621.js

--- js/983621.js
+++ js/983621.js
@@ -1,30 +1,30 @@
-var link1 = "demo1.html";
-var link2 = "demo1.html";
-var link3 = "demo1.html";
-var link4 = "demo1.html";
-var link5 = "demo1.html";
+var updateLink1 = "usinginlinefornavigation.html";
+var updateLink2 = "#";
+var updateLink3 = "configurenetworkondebian.html";
+var updateLink4 = "privacy.html";
+var updateLink5 = "index.html";
 
-document.getElementById("update-head-1").innerHTML = "<a href='" + link1 + "'>Test Title 1...</a>";
-document.getElementById("update-date-1").innerHTML = "<a href='" + link1 + "'>date 1...</a>";
-document.getElementById("update-info-1").innerHTML = "<a href='" + link1 + "'>This is test data 1...</a>";
-document.getElementById("update-img-1").innerHTML = "<a href='" + link1 + "'><img src='media/images/test/test4.png' alt='test'></a>";
+document.getElementById("update-head-1").innerHTML = "<a href='" + updateLink1 + "'>Using In line Display Method for Bar Navigation</a>";
+document.getElementById("update-date-1").innerHTML = "<a href='" + updateLink1 + "'>11th October 2021</a>";
+document.getElementById("update-info-1").innerHTML = "<a href='" + updateLink1 + "'>Using the inline display method for creating near and responsive bar navigation panels.</a>";
+document.getElementById("update-img-1").innerHTML = "<a href='" + updateLink1 + "'><img src='media/images/programming/blog/UsingInlineDisplayMethod/inline_preview.png' alt='inline navigation'></a>";
 
-document.getElementById("update-head-2").innerHTML = "<a href='" + link2 + "'>Test Title 1...</a>";
-document.getElementById("update-date-2").innerHTML = "<a href='" + link2 + "'>date 1...</a>";
-document.getElementById("update-info-2").innerHTML = "<a href='" + link2 + "'>This is test data 1...</a>";
-document.getElementById("update-img-2").innerHTML = "<a href='" + link2 + "'><img src='media/images/test/test4.png' alt='test'></a>";
+document.getElementById("update-head-2").innerHTML = "<a href='" + updateLink2 + "'>Coming Soon</a>";
+document.getElementById("update-date-2").innerHTML = "<a href='" + updateLink2 + "'></a>";
+document.getElementById("update-info-2").innerHTML = "<a href='" + updateLink2 + "'>Coming Soon</a>";
+document.getElementById("update-img-2").innerHTML = "<a href='" + updateLink2 + "'><img src='media/images/computing/blog/default/tjhcomp_preview.png' alt='TJH Comp Preview'></a>";
 
-document.getElementById("update-head-3").innerHTML = "<a href='" + link3 + "'>Test Title 1...</a>";
-document.getElementById("update-date-3").innerHTML = "<a href='" + link3 + "'>date 1...</a>";
-document.getElementById("update-info-3").innerHTML = "<a href='" + link3 + "'>This is test data 1...</a>";
-document.getElementById("update-img-3").innerHTML = "<a href='" + link3 + "'><img src='media/images/test/test4.png' alt='test'></a>";
+document.getElementById("update-head-3").innerHTML = "<a href='" + updateLink3 + "'>Configuring Network on Debian</a>";
+document.getElementById("update-date-3").innerHTML = "<a href='" + updateLink3 + "'>11th October 2021</a>";
+document.getElementById("update-info-3").innerHTML = "<a href='" + updateLink3 + "'>A quick guide on how to configure Static and DHCP Networking.</a>";
+document.getElementById("update-img-3").innerHTML = "<a href='" + updateLink3 + "'><img src='media/images/computing/blog/default/tjhcomp_preview.png' alt='TJH Comp Preview'></a>";
 
-document.getElementById("update-head-4").innerHTML = "<a href='" + link4 + "'>Test Title 1...</a>";
-document.getElementById("update-date-4").innerHTML = "<a href='" + link4 + "'>date 1...</a>";
-document.getElementById("update-info-4").innerHTML = "<a href='" + link4 + "'>This is test data 1...</a>";
-document.getElementById("update-img-4").innerHTML = "<a href='" + link4 + "'><img src='media/images/test/test4.png' alt='test'></a>";
+document.getElementById("update-head-4").innerHTML = "<a href='" + updateLink4 + "'>Don't Forget About Important Stuff!</a>";
+document.getElementById("update-date-4").innerHTML = "<a href='" + updateLink4 + "'>11th October 2021</a>";
+document.getElementById("update-info-4").innerHTML = "<a href='" + updateLink4 + "'>Don't forget to read about the privacy and guidelines about this website! Click this space to go there!</a>";
+document.getElementById("update-img-4").innerHTML = "<a href='" + updateLink4 + "'><img src='media/images/computing/blog/default/tjhcomp_preview.png' alt='TJH Comp Preview'></a>";
 
-document.getElementById("update-head-5").innerHTML = "<a href='" + link5 + "'>Test Title 1...</a>";
-document.getElementById("update-date-5").innerHTML = "<a href='" + link5 + "'>date 1...</a>";
-document.getElementById("update-info-5").innerHTML = "<a href='" + link5 + "'>This is test data 1...</a>";
-document.getElementById("update-img-5").innerHTML = "<a href='" + link5 + "'><img src='media/images/test/test4.png' alt='test'></a>";
+document.getElementById("update-head-5").innerHTML = "<a href='" + updateLink5 + "'>Welcome to TJHComp!</a>";
+document.getElementById("update-date-5").innerHTML = "<a href='" + updateLink5 + "'>11th October 2021...</a>";
+document.getElementById("update-info-5").innerHTML = "<a href='" + updateLink5 + "'>A newly created website which will help computing audiences with my knoweldge and ideas behind computing. Remember, you can follow my social media profiles! On your left!<br><br>As a new website, some  content may be missing or website has loss of functionality. Be patient!</a>";
+document.getElementById("update-img-5").innerHTML = "<a href='" + updateLink5 + "'><img src='media/images/computing/blog/default/tjhcomp_preview.png' alt='TJH Comp Preview'></a>";
