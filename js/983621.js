0a89123441b6410bb32aa52fb0d35be2884d5e82
tjhcomputing <86693306+tjhcomputing@users.noreply.github.com>
2021-10-11 02:37:46 +0100

    Create 983621.js

---
+++ js/983621.js
@@ -0,0 +1,30 @@
+var link1 = "demo1.html";
+var link2 = "demo1.html";
+var link3 = "demo1.html";
+var link4 = "demo1.html";
+var link5 = "demo1.html";
+
+document.getElementById("update-head-1").innerHTML = "<a href='" + link1 + "'>Test Title 1...</a>";
+document.getElementById("update-date-1").innerHTML = "<a href='" + link1 + "'>date 1...</a>";
+document.getElementById("update-info-1").innerHTML = "<a href='" + link1 + "'>This is test data 1...</a>";
+document.getElementById("update-img-1").innerHTML = "<a href='" + link1 + "'><img src='media/images/test/test4.png' alt='test'></a>";
+
+document.getElementById("update-head-2").innerHTML = "<a href='" + link2 + "'>Test Title 1...</a>";
+document.getElementById("update-date-2").innerHTML = "<a href='" + link2 + "'>date 1...</a>";
+document.getElementById("update-info-2").innerHTML = "<a href='" + link2 + "'>This is test data 1...</a>";
+document.getElementById("update-img-2").innerHTML = "<a href='" + link2 + "'><img src='media/images/test/test4.png' alt='test'></a>";
+
+document.getElementById("update-head-3").innerHTML = "<a href='" + link3 + "'>Test Title 1...</a>";
+document.getElementById("update-date-3").innerHTML = "<a href='" + link3 + "'>date 1...</a>";
+document.getElementById("update-info-3").innerHTML = "<a href='" + link3 + "'>This is test data 1...</a>";
+document.getElementById("update-img-3").innerHTML = "<a href='" + link3 + "'><img src='media/images/test/test4.png' alt='test'></a>";
+
+document.getElementById("update-head-4").innerHTML = "<a href='" + link4 + "'>Test Title 1...</a>";
+document.getElementById("update-date-4").innerHTML = "<a href='" + link4 + "'>date 1...</a>";
+document.getElementById("update-info-4").innerHTML = "<a href='" + link4 + "'>This is test data 1...</a>";
+document.getElementById("update-img-4").innerHTML = "<a href='" + link4 + "'><img src='media/images/test/test4.png' alt='test'></a>";
+
+document.getElementById("update-head-5").innerHTML = "<a href='" + link5 + "'>Test Title 1...</a>";
+document.getElementById("update-date-5").innerHTML = "<a href='" + link5 + "'>date 1...</a>";
+document.getElementById("update-info-5").innerHTML = "<a href='" + link5 + "'>This is test data 1...</a>";
+document.getElementById("update-img-5").innerHTML = "<a href='" + link5 + "'><img src='media/images/test/test4.png' alt='test'></a>";
