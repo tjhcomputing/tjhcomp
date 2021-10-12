2aee741c9bc601499363864b5e01673d7a3278c0
tjhcomputing <86693306+tjhcomputing@users.noreply.github.com>
2021-10-12 12:33:29 +0100

    Create 477441.js

---
+++ js/477441.js
@@ -0,0 +1,30 @@
+var updateLink1 = "creating_inline_bar_nav.html";
+var updateLink2 = "understanding_frontend_development.html";
+var updateLink3 = "#";
+var updateLink4 = "#";
+var updateLink5 = "#";
+
+document.getElementById("update-head-1").innerHTML = "<a href='" + updateLink1 + "'>Creating Bar Navigation Using Inline Display</a>";
+document.getElementById("update-date-1").innerHTML = "<a href='" + updateLink1 + "'>11th October 2021</a>";
+document.getElementById("update-info-1").innerHTML = "<a href='" + updateLink1 + "'>Using the inline display method for creating neat and responsive bar navigation panels.</a>";
+document.getElementById("update-img-1").innerHTML = "<a href='" + updateLink1 + "'><img src='media/images/programming/blog/UsingInlineDisplayMethod/inline_preview.png' alt='inline navigation'></a>";
+
+document.getElementById("update-head-2").innerHTML = "<a href='" + updateLink2 + "'>The Understanding of Frontend Development</a>";
+document.getElementById("update-date-2").innerHTML = "<a href='" + updateLink2 + "'>11th October 2021</a>";
+document.getElementById("update-info-2").innerHTML = "<a href='" + updateLink2 + "'>Developing the understanding about what front-end development means and why we need to have front-end.</a>";
+document.getElementById("update-img-2").innerHTML = "<a href='" + updateLink2 + "'><img src='media/images/programming/guides/frontend_preview.png' alt='Frontend Preview'></a>";
+
+document.getElementById("update-head-3").innerHTML = "<a href='" + updateLink3 + "'>Coming Soon</a>";
+document.getElementById("update-date-3").innerHTML = "<a href='" + updateLink3 + "'></a>";
+document.getElementById("update-info-3").innerHTML = "<a href='" + updateLink3 + "'>Content Coming Soon.</a>";
+document.getElementById("update-img-3").innerHTML = "<a href='" + updateLink3 + "'><img src='media/images/computing/blog/default/tjhcomp_preview.png' alt='TJH Comp Preview'></a>";
+
+document.getElementById("update-head-4").innerHTML = "<a href='" + updateLink4 + "'>Coming Soon</a>";
+document.getElementById("update-date-4").innerHTML = "<a href='" + updateLink4 + "'></a>";
+document.getElementById("update-info-4").innerHTML = "<a href='" + updateLink4 + "'>Content Coming Soon.</a>";
+document.getElementById("update-img-4").innerHTML = "<a href='" + updateLink4 + "'><img src='media/images/computing/blog/default/tjhcomp_preview.png' alt='TJH Comp Preview'></a>";
+
+document.getElementById("update-head-5").innerHTML = "<a href='" + updateLink5 + "'>Coming Soon</a>";
+document.getElementById("update-date-5").innerHTML = "<a href='" + updateLink5 + "'></a>";
+document.getElementById("update-info-5").innerHTML = "<a href='" + updateLink5 + "'>Content Coming Soon.</a>";
+document.getElementById("update-img-5").innerHTML = "<a href='" + updateLink5 + "'><img src='media/images/computing/blog/default/tjhcomp_preview.png' alt='TJH Comp Preview'></a>";
