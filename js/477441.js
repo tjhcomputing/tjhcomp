f87c5fe6d4ada010e03ff02ad70dc03458ebf990
tjhcomputing <86693306+tjhcomputing@users.noreply.github.com>
2021-10-13 18:54:23 +0100

    Update 477441.js

--- js/477441.js
+++ js/477441.js
@@ -7,7 +7,7 @@ var updateLink5 = "#";
 document.getElementById("update-head-1").innerHTML = "<a href='" + updateLink1 + "'>Creating Bar Navigation Using Inline Display</a>";
 document.getElementById("update-date-1").innerHTML = "<a href='" + updateLink1 + "'>11th October 2021</a>";
 document.getElementById("update-info-1").innerHTML = "<a href='" + updateLink1 + "'>Using the inline display method for creating neat and responsive bar navigation panels.</a>";
-document.getElementById("update-img-1").innerHTML = "<a href='" + updateLink1 + "'><img src='media/images/programming/blog/inlinemethod/inlinepreview.png' alt='inline navigation'></a>";
+document.getElementById("update-img-1").innerHTML = "<a href='" + updateLink1 + "'><img src='media/images/preview/inlinepreview.PNG' alt='inline navigation'></a>";
 
 document.getElementById("update-head-2").innerHTML = "<a href='" + updateLink2 + "'>Coming Soon</a>";
 document.getElementById("update-date-2").innerHTML = "<a href='" + updateLink2 + "'></a>";
