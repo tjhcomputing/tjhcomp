a3c818d3eeac8c98ccf9be4d3b72435e2e8702b3
tjhcomputing <86693306+tjhcomputing@users.noreply.github.com>
2021-10-13 18:47:48 +0100

    Update 983621.js

--- js/983621.js
+++ js/983621.js
@@ -1,7 +1,7 @@
-var updateLink1 = "creating_inline_bar_nav.html#20931";
-var updateLink2 = "understanding_frontend_development.html#32847";
+var updateLink1 = "creating_inline_bar_nav.html";
+var updateLink2 = "#";
 var updateLink3 = "#";
-var updateLink4 = "privacy.html";
+var updateLink4 = "#";
 var updateLink5 = "index.html";
 
 document.getElementById("update-head-1").innerHTML = "<a href='" + updateLink1 + "'>Creating Bar Navigation Using Inline Display</a>";
@@ -9,10 +9,10 @@ document.getElementById("update-date-1").innerHTML = "<a href='" + updateLink1 +
 document.getElementById("update-info-1").innerHTML = "<a href='" + updateLink1 + "'>Using the inline display method for creating neat and responsive bar navigation panels.</a>";
 document.getElementById("update-img-1").innerHTML = "<a href='" + updateLink1 + "'><img src='media/images/programming/blog/inlinemethod/inlinepreview.png' alt='inline navigation'></a>";
 
-document.getElementById("update-head-2").innerHTML = "<a href='" + updateLink2 + "'>The Understanding of Frontend Development</a>";
-document.getElementById("update-date-2").innerHTML = "<a href='" + updateLink2 + "'>11th October 2021</a>";
-document.getElementById("update-info-2").innerHTML = "<a href='" + updateLink2 + "'>Developing the understanding about what front-end development means and why we need to have front-end.</a>";
-document.getElementById("update-img-2").innerHTML = "<a href='" + updateLink2 + "'><img src='media/images/programming/guides/frontendpreview.png' alt='Frontend Preview'></a>";
+document.getElementById("update-head-2").innerHTML = "<a href='" + updateLink2 + "'>Coming Soon</a>";
+document.getElementById("update-date-2").innerHTML = "<a href='" + updateLink2 + "'></a>";
+document.getElementById("update-info-2").innerHTML = "<a href='" + updateLink2 + "'>Coming Soon</a>";
+document.getElementById("update-img-2").innerHTML = "<a href='" + updateLink2 + "'><img src='media/images/computing/blog/default/tjhcomp_preview.png' alt='TJH Comp Preview'></a>";
 
 document.getElementById("update-head-3").innerHTML = "<a href='" + updateLink3 + "'>Coming Soon</a>";
 document.getElementById("update-date-3").innerHTML = "<a href='" + updateLink3 + "'></a>";
