bc43ca1a6ae3e6e25b9b59acc5dfe8824b417219
tjhcomputing <86693306+tjhcomputing@users.noreply.github.com>
2021-10-19 03:05:38 +0100

    Create 823111.js

---
+++ js/823111.js
@@ -0,0 +1,93 @@
+// Global Sectors
+var defaultimage = "<img src='media/images/default/tjhcomp_preview.png'>";
+
+// Update sector 1
+var title1 = "Create A Bar Navigation With Inline Display";
+var date1 = month[9] + " " + day[14] + " " + year[5];
+var details1 = "Explaining the concept of using <b>Display</b> properties to improve responsive techniques.";
+var linktags1 = htmlTag + ", " + cssTag + ", " + navTag;
+var image1 = "<img src='media/images/previews/tjh_nav_display_160x160.png'>";
+var defaultimage = "<img src='media/images/default/tjhcomp_preview.png'>";
+
+var link1 = "creating_inline_bar_nav.html";
+
+document.getElementById("update-title-1").innerHTML = "<a href='" + link1 + "'>" + title1 + "</a>";
+document.getElementById("update-date-1").innerHTML = "<a href='" + link1 + "'>" + date1 + "</a>";
+document.getElementById("update-details-1").innerHTML = "<a href='" + link1 + "'>" + details1 + "</a>";
+document.getElementById("update-tags-1").innerHTML = "<a href='" + link1 + "'>" + linktags1 + "</a>";
+document.getElementById("update-image-1").innerHTML = "<a href='" + link1 + "'>" + image1 + "</a>";
+
+// Update sector 2
+var title2 = "Coming Soon";
+var date2 = "";
+var details2 = "Coming Soon";
+var linktags2 = "";
+//var image2 = "";
+
+var link2 = "";
+
+document.getElementById("update-title-2").innerHTML = "<a href='" + link2 + "'>" + title2 + "</a>";
+document.getElementById("update-date-2").innerHTML = "<a href='" + link2 + "'>" + date2 + "</a>";
+document.getElementById("update-details-2").innerHTML = "<a href='" + link2 + "'>" + details2 + "</a>";
+document.getElementById("update-tags-2").innerHTML = "<a href='" + link2 + "'>" + linktags2 + "</a>";
+document.getElementById("update-image-2").innerHTML = "<a href='" + link2 + "'>" + defaultimage + "</a>";
+
+// Update sector 3
+var title3 = "Coming Soon";
+var date3 = "";
+var details3 = "Coming Soon";
+var linktags3 = "";
+//var image3 = "";
+
+var link3 = "";
+
+document.getElementById("update-title-3").innerHTML = "<a href='" + link3 + "'>" + title3 + "</a>";
+document.getElementById("update-date-3").innerHTML = "<a href='" + link3 + "'>" + date3 + "</a>";
+document.getElementById("update-details-3").innerHTML = "<a href='" + link3 + "'>" + details3 + "</a>";
+document.getElementById("update-tags-3").innerHTML = "<a href='" + link3 + "'>" + linktags3 + "</a>";
+document.getElementById("update-image-3").innerHTML = "<a href='" + link3 + "'>" + defaultimage + "</a>";
+
+// Update sector 4
+var title4 = "Coming Soon";
+var date4 = "";
+var details4 = "Coming Soon";
+var linktags4 = "";
+//var image4 = "";
+
+var link4 = "";
+
+document.getElementById("update-title-4").innerHTML = "<a href='" + link4 + "'>" + title4 + "</a>";
+document.getElementById("update-date-4").innerHTML = "<a href='" + link4 + "'>" + date4 + "</a>";
+document.getElementById("update-details-4").innerHTML = "<a href='" + link4 + "'>" + details4 + "</a>";
+document.getElementById("update-tags-4").innerHTML = "<a href='" + link4 + "'>" + linktags4 + "</a>";
+document.getElementById("update-image-4").innerHTML = "<a href='" + link4 + "'>" + defaultimage + "</a>";
+
+// Update sector 5
+var title5 = "Coming Soon";
+var date5 = "";
+var details5 = "Coming Soon";
+var linktags5 = "";
+//var image5 = "";
+
+var link5 = "";
+
+document.getElementById("update-title-5").innerHTML = "<a href='" + link5 + "'>" + title5 + "</a>";
+document.getElementById("update-date-5").innerHTML = "<a href='" + link5 + "'>" + date5 + "</a>";
+document.getElementById("update-details-5").innerHTML = "<a href='" + link5 + "'>" + details5 + "</a>";
+document.getElementById("update-tags-5").innerHTML = "<a href='" + link5 + "'>" + linktags5 + "</a>";
+document.getElementById("update-image-5").innerHTML = "<a href='" + link5 + "'>" + defaultimage + "</a>";
+
+// Update sector 6
+var title6 = "Coming Soon";
+var date6 = "";
+var details6 = "Coming Soon";
+var linktags6 = "";
+//var image6 = "";
+
+var link6 = "";
+
+document.getElementById("update-title-6").innerHTML = "<a href='" + link6 + "'>" + title6 + "</a>";
+document.getElementById("update-date-6").innerHTML = "<a href='" + link6 + "'>" + date6 + "</a>";
+document.getElementById("update-details-6").innerHTML = "<a href='" + link6 + "'>" + details6 + "</a>";
+document.getElementById("update-tags-6").innerHTML = "<a href='" + link6 + "'>" + linktags6 + "</a>";
+document.getElementById("update-image-6").innerHTML = "<a href='" + link6 + "'>" + defaultimage + "</a>";
